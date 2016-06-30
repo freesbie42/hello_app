cf73c0c73f4115742b02dfc9a418a079441543d3
freesbie42 <freesbie1973@gmail.com>
2016-06-30 19:20:23 +0200

    add goodby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "¡holla, mundo!"
+
+  end
+
+  def goodby
+    render html: "goodby, world!"
 
   end
 
